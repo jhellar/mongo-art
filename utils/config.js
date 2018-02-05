831b46fc8784f35af061e98a41cf3bb6d9502d3c
spahuta <spahuta@redhat.com>
2018-02-05 15:51:37 +0100

    Create some mbaas and forms support functions

--- utils/config.js
+++ utils/config.js
@@ -9,12 +9,14 @@ const argv = yargs
   .alias('h', 'host')
   .alias('u', 'user')
   .alias('p', 'pass')
+  .alias('s', 'servicekey')
   .alias('e', 'env')
   .alias('r', 'runtime')
   .alias('x', 'prefix')
   .describe('host', 'RHMAP host')
   .describe('user', 'RHMAP username')
   .describe('pass', 'RHMAP password')
+  .describe('servicekey', 'MBaaS servicekey')
   .describe('env', 'RHMAP environment')
   .describe('runtime', 'Cloud App runtime')
   .describe('prefix', 'Prefix for artifacts')
@@ -24,6 +26,7 @@ const config = {
   host: argv.host,
   user: argv.user,
   pass: argv.pass,
+  servicekey: argv.servicekey,
   env: argv.env,
   runtime: argv.runtime,
   prefix: argv.prefix
