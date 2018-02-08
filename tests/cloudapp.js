eab69f8f417a403e7eba33cc47b56ab9ea34f2dc
Jan Hellar <jhellar@redhat.com>
2018-02-08 12:30:06 +0100

    Find primary mongo

--- tests/cloudapp.js
+++ tests/cloudapp.js
@@ -1,5 +1,6 @@
 const request = require('request-promise-native');
 const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
 const config = require('../config');
 const fhc = require('../utils/fhc');
 const services = require('../services/cloudapp');
@@ -9,6 +10,7 @@ describe('Cloud App', function() {
   this.timeout(0);
 
   let project;
+  let userClient;
 
   before(async function() {
     /*
@@ -33,46 +35,78 @@ describe('Cloud App', function() {
     */
 
     // create project
-    await fhc.init(config.host, config.user, config.pass);
-    project = await fhc.createProject(`${config.prefix}${Date.now()}`, 'bare_project');
-    const { form, theme } = await services.createForm(project.guid);
+    // await fhc.init(config.host, config.user, config.pass);
+    // project = await fhc.createProject(`${config.prefix}${Date.now()}`, 'bare_project');
 
-    // cloud app
-    const zipFile = path.resolve(__dirname, '../cloudapp.zip')
-    services.zipFolder(path.resolve(__dirname, '../cloud_app'), zipFile);
-    const cloudApp = await fhc.importAppZip('Cloud App', project.guid, zipFile, 'cloud_nodejs');
-    try {
-      await fhc.appDeploy(cloudApp.guid, config.env, config.runtime);
-    } catch (_) {
-      await fhc.appDeploy(cloudApp.guid, config.env, config.runtime);
-    }
-    const cloudAppUrl = await fhc.getCloudUrl(cloudApp.guid, config.env);
-    await new Promise(resolve => setTimeout(resolve, 60000));
-    const mongoUrl = await request({ uri: `${cloudAppUrl}/test`, json: true });
-    console.log(mongoUrl);
+    // // create form and theme and associate it with the project
+    // const { form, theme } = await services.createForm(project.guid);
 
-    // client app
-    const clientApp = await fhc.importApp(project.guid, 'Client App', 'client_advanced_hybrid', 'https://github.com/feedhenry-templates/helloworld-app.git', 'master', config.env);
-    const connections = await fhc.connectionsList(project.guid);
-    services.createFHConfigFile(clientApp, project, config.host, connections[0]);
-    await services.runClientApp();
+    // // cloud app
+    // const zipFile = path.resolve(__dirname, '../cloudapp.zip')
+    // services.zipFolder(path.resolve(__dirname, '../cloud_app'), zipFile);
+    // const cloudApp = await fhc.importAppZip('Cloud App', project.guid, zipFile, 'cloud_nodejs');
+    // try {
+    //   await fhc.appDeploy(cloudApp.guid, config.env, config.runtime);
+    // } catch (_) {
+    //   await fhc.appDeploy(cloudApp.guid, config.env, config.runtime);
+    // }
+    // const cloudAppUrl = await fhc.getCloudUrl(cloudApp.guid, config.env);
+    // await new Promise(resolve => setTimeout(resolve, 60000));
+    // const mongoUrl = await request({ uri: `${cloudAppUrl}/test`, json: true });
+    // console.log(mongoUrl);
 
-    // form submission
-    var options = {
-      url: `${cloudAppUrl}/submission`,
-      method: 'POST',
-      json: { form, theme, clientApp }
-    };
-    const response = await request(options);
-    console.log(response);
+    // // client app
+    // const clientApp = await fhc.importApp(project.guid, 'Client App', 'client_advanced_hybrid', 'https://github.com/feedhenry-templates/helloworld-app.git', 'master', config.env);
+    // const connections = await fhc.connectionsList(project.guid);
+    // services.createFHConfigFile(clientApp, project, config.host, connections[0]);
+    // await services.runClientApp();
+
+    // // form submission
+    // var options = {
+    //   url: `${cloudAppUrl}/submission`,
+    //   method: 'POST',
+    //   json: { form, theme, clientApp }
+    // };
+    // const response = await request(options);
+    // console.log(response);
+
+    // TODO: get system and user space mongo pod name
+
+    // TODO: portforward both
+
+    // TODO: create connections to mongos
+    const userUrl = 'mongodb://admin:uD3DIIhmugC3eQbFCEaMP06DjqtHOiBB5al7Tltw@localhost:27017';
+    userClient = await MongoClient.connect(userUrl);
+
+    // TODO: get cloud app pod name
+
   });
 
   after(async function() {
+    // TODO: close mongo connections
+    userClient.close();
+
+    // delete project
     // await fhc.projectDelete(project.guid);
+    
+    // TODO: delete form and theme
   });
 
-  it('should save form to user space mongo', function() {
-    
+  it(`user space mongo should contain cloud app's database`, async function() {
+    const db = userClient.db('rhmap_dev');
+    await db.collections();
+  });
+
+  it(`cloud app's database should contain data created with fh.db`, async function() {
+  });
+
+  it(`cloud app's database should contain data created with sync`, async function() {
+  });
+
+  it(`user space mongo should contain forms database`, async function() {
+  });
+
+  it(`forms database should contain submission`, async function() {
   });
 
 });
